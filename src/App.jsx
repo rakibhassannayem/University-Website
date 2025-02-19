1da913f760744a4d1a60d3e6fb63dc8ee2493912
rakibhassannayem <rakibhassannayem@gmail.com>
2025-02-19 22:58:20 +0600

    all up

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import About from "./components/About/About";
+import Campus from "./components/Campus/Campus";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Programs from "./components/Programs/Programs";
+import Testimonials from "./components/Tesmonials/Testimonials";
+import Title from "./components/Title/Title";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+
+const App = () => {
+  
+  const [playState, setPlayState] = useState(false);
+
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+        <Title subTitle='Our PROGRAMS' title='What We offer'/>
+        <Programs/>
+        <About setPlayState={setPlayState}/>
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+        <Testimonials/>
+        <Title subTitle='contact us' title='Get in Touch'/>
+        <Contact/>
+        <Footer/>
+      </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
+    </div>
+  );
+};
+
+export default App;
